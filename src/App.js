938a04ef012186dd74509793e480c021047d5b1e
JayMercado <jay.gutierrezmercado@gmail.com>
2021-10-27 20:59:09 +0800

    Feature: Added AboutMe Component

--- src/App.js
+++ src/App.js
@@ -9,24 +9,26 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 import Jumbotron from './components/Jumbo'
 import Footer from './components/Footer'
+import AboutMe from './components/AboutMe'
 
 // Bootstrap Components
 import { Container, Row, Col } from 'react-bootstrap'
 
 
-export default function App(){
+export default function App() {
 	return (
 		<>
-			<Navbar/>
-			<Jumbotron/>
+			<Navbar />
+			<Jumbotron />
 			<Container>
 				<Row>
-					<Col><Card/></Col>
-					<Col><Card/></Col>
-					<Col><Card/></Col>
+					<Col><Card /></Col>
+					<Col><Card /></Col>
+					<Col><Card /></Col>
 				</Row>
 			</Container>
-			<Footer/>
+			<AboutMe />
+			<Footer />
 		</>
 	)
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,19 @@
+// React Library
+import React from 'react'
+
+// Bootstrap Components
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+export default function AboutMe() {
+	return (
+		<Jumbotron>
+			<h1>Rodolfo Junior Mercado!</h1>
+			<ul>
+				<li>I'm a Full Stack Developer.</li>
+				<li>I live in the Philippines.</li>
+				<li>email: jay.gutierrezmercado@gmail.com</li>
+				<li>contact no.: 0967 258 8825</li>
+			</ul>
+		</Jumbotron>
+	)
+}
